600390f1bd7fb76b2429f5a2c17645fcbc2be851
TejasRDalal <tejas281990@gmail.com>
2025-10-22 16:22:30 +0530

    Added @Transactional annotation on TaskServiceImpl.java

--- src/main/java/com/taskmanager/task/service/impl/TaskServiceImpl.java
+++ src/main/java/com/taskmanager/task/service/impl/TaskServiceImpl.java
@@ -3,12 +3,14 @@ package com.taskmanager.task.service.impl;
 import com.taskmanager.task.pojo.Task;
 import com.taskmanager.task.repositories.TaskRepository;
 import com.taskmanager.task.service.TaskService;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 @Service
 public class TaskServiceImpl implements TaskService {
 
